ba7d49d8bb4f89d74d7cb9d29e406e767fad3529
waweru-sdf <felixwaweru200@gmail.com>
2025-08-27 10:30:54 +0300

    add new participant

--- crud.py
+++ crud.py
@@ -2,7 +2,15 @@ from models import Organizer, Event, EventParticipant, Participant,session
 
 
 
-def add_new_organizer( organizer_id id ,name ,email, phone,age )
-    new_organizer= Organizer(organizer_id=organizer_id ,name=name ,email=email, phone=phone,age=age )
+def add_new_organizer( organizer_id  ,name ,email, phone,organization )
+    new_organizer= Organizer(organizer_id=organizer_id ,name=name ,email=email, phone=phone,organization=organization )
     session.add(new_organizer)
     session.commit()
+
+
+def add_new_participant( event_id  ,name ,email, phone,age )
+    new_participant=Participant(organizer_id=organizer_id ,name=name ,email=email, phone=phone,age=age )
+    session.add(new_participant)
+    session.commit()
+
+
