4d6d1b09e623002fd3d8f7f9350be69ae5b2775c
waweru-sdf <felixwaweru200@gmail.com>
2025-08-27 10:05:25 +0300

    EventParticipant and Participant table

--- models.py
+++ models.py
@@ -19,3 +19,32 @@ class Event(Base):
     description=Column(Text)
     date=Column(DateTime)
     location=Column(String)
+    organizer_id=Column(Integer,ForeignKey('organaizer.id'))
+
+
+    organizer= relationship('Organizer' back_populates='employees')
+
+class EventParticipant(Base):
+    _tablename_="eventparticipant"
+    id=Column(Integer,primary_key=True)
+    event_id=Column(Integer,ForeignKey('event'))
+    Participant_id=Column(Integer,ForeignKey('participant'))
+    role=Column(String)
+    registration_dat=Column(DateTime)
+
+    event = relationship('Event',back_populates='employees')
+    participant=relationship('Participant',back_populates='participants')
+
+
+class Participant(Base):
+    _tablename_="participant"
+    id=Column(Integer,primary_key=True)
+    name=ColumnI(String(20))
+    email=Column(String(40))
+    phone=Column(Integer)
+    age=Column(Integer)
+    event_id=Column(Integer,ForeignKey('event'))
+
+    event=relationship('Event',back_populates='events')
+    
+
