7906675fa05c99ea8f19ef28741a23e29eb6d686
waweru-sdf <felixwaweru200@gmail.com>
2025-08-27 10:54:05 +0300

    add new event function

--- crud.py
+++ crud.py
@@ -14,3 +14,8 @@ def add_new_participant( event_id  ,name ,email, phone,age )
     session.commit()
 
 
+def add_new_event(event_id,title,description,date,location,organization_id=none)
+    new_event=Event(event_id=event_id,title=title,description=description,date=date,location=location,organization_id=organization_id)
+    session.add(new_event)
+    session.commit()
+    
